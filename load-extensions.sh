cdfbd181b752264d64d02acbf1046dbffeb49a04
c-e-p <c.elena.palmer@outlook.com>
2026-07-11 18:04:38 -0500

    Update load-extensions.sh

--- load-extensions.sh
+++ load-extensions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB"  <<-EOSQL
      create extension pg_trgm;
-	 select * FROM pg_extension;
+	select * FROM pg_extension;
 EOSQL
